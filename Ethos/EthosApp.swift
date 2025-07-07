c3549a356a1f8abd28338a5191cfb5d87b5558dd
Soong En Wong <wsoongen2015@gmail.com>
2025-07-07 22:20:37 +0100

    Initial Commit

---
+++ Ethos/EthosApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthosApp.swift
+//  Ethos
+//
+//  Created by Soong En Wong on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct EthosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EthosTests/EthosTests.swift
@@ -0,0 +1,17 @@
+//
+//  EthosTests.swift
+//  EthosTests
+//
+//  Created by Soong En Wong on 07/07/2025.
+//
+
+import Testing
+@testable import Ethos
+
+struct EthosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
